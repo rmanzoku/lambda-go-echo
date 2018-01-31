a078ec58931a29b2cf1484f9bd1a01b4b42c58f4
Ryo Manzoku <manzoku.ryo@gmail.com>
2018-01-31 06:57:36 +0000

    AWS SAM local

--- Makefile
+++ Makefile
@@ -1,4 +1,10 @@
 
+run_local:
+	aws-sam-local local invoke --event event.json
+
+setup:
+	go get -u github.com/awslabs/aws-sam-local
+
 
 build:
 	GOOS=linux go build -o main
--- main.go
+++ main.go
@@ -1,38 +1,22 @@
 package main
 
 import (
-	"errors"
-	"log"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+        "fmt"
+        "log"
+        "context"
+        "github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// ErrNameNotProvided is thrown when a name is not provided
-	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
-)
+type Event map[string]interface{}
 
-// Handler is your Lambda function handler
-// It uses Amazon API Gateway request/responses provided by the aws-lambda-go/events package,
-// However you could use other event sources (S3, Kinesis etc), or JSON-decoded primitive types such as 'string'.
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func HandleRequest(ctx context.Context, event Event) (string, error) {
+        log.Print("value1 = ", event["key1"] )
+        log.Print("value2 = ", event["key2"] )
+        log.Print("value3 = ", event["key3"] )
 
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
-
-	// If no name is provided in the HTTP request body, throw an error
-	if len(request.Body) < 1 {
-		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       "Hello " + request.Body,
-		StatusCode: 200,
-	}, nil
-
+        return fmt.Sprintf("Hello World"), nil
 }
 
 func main() {
-	lambda.Start(Handler)
+        lambda.Start(HandleRequest)
 }
